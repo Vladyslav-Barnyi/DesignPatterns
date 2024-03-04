cbd1834877c0d4e9eeeee4bcc7eeaa80b4e971ee
vbarnyi <vladyslavbarnyi@gmail.com>
2024-03-04 12:01:42 +0100

    add Strategy

---
+++ Strategy/TravelPlanner.cs
@@ -0,0 +1,19 @@
+﻿using Strategy.Strategies;
+
+namespace Strategy;
+
+// Context class that uses a travel strategy
+public class TravelPlanner
+{
+    private ITravelStrategy travelStrategy;
+
+    public void SetTravelStrategy(ITravelStrategy strategy)
+    {
+        travelStrategy = strategy;
+    }
+
+    public void PlanTravel(string source, string destination)
+    {
+        travelStrategy.Travel(source, destination);
+    }
+}
