de277bedbf9b2805e79116ea635fa16564f8097d
vbarnyi <vladyslavbarnyi@gmail.com>
2024-03-04 11:48:28 +0100

    add Observer

---
+++ Observer/Station/WeatherStation.cs
@@ -0,0 +1,39 @@
+﻿namespace Observer.Station;
+using IObserver = Observer.Observers.IObserver;
+
+// Concrete weather station class
+public class WeatherStation : IWeatherStation
+{
+    private List<IObserver> observers = new List<IObserver>();
+    private string weatherUpdate;
+
+    public void AddObserver(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void NotifyObservers()
+    {
+        var x = 1;
+        foreach (var observer in observers)
+        {
+            observer.Update(weatherUpdate);
+
+            //Simulation
+            Thread.Sleep(1000);
+            Console.WriteLine($"Update for {observer} n.{x}");
+            x++;
+        }
+    }
+
+    public void SetWeatherUpdate(string update)
+    {
+        weatherUpdate = update;
+        NotifyObservers();
+    }
+}
