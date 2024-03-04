aeb591b56031c2fa663fd82e37387faefb70502a
vbarnyi <vladyslavbarnyi@gmail.com>
2024-03-04 11:18:15 +0100

    add singleton

---
+++ Singleton/PrintSpoolerThreadSafe.cs
@@ -0,0 +1,33 @@
+﻿namespace Singleton;
+
+public class PrintSpoolerThreadSafe
+{
+    private static volatile PrintSpoolerThreadSafe instance;
+    private static readonly object SyncRoot = new ();
+
+    private PrintSpoolerThreadSafe()
+    {
+    }
+
+    public static PrintSpoolerThreadSafe GetInstance()
+    {
+        if (instance is null)
+        {
+            lock (SyncRoot)
+            {
+                if (instance is null)
+                {
+                    instance = new PrintSpoolerThreadSafe();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void Print(string document)
+    {
+        // Simulate printing logic.
+        Console.WriteLine($"Printing: {document}");
+    }
+}
